20c198190cfc2da87c1bea3fd660b452b568a8c9
Aiden Nicolas <Nicolas.Aiden@SEMS259.LAB>
2020-01-15 16:21:48 -0800

    loll
    awdasdw

--- src/_04_validation/Validation.java
+++ src/_04_validation/Validation.java
@@ -10,25 +10,27 @@ import javax.swing.JOptionPane;
 public class Validation {
 	public static void main(String[] args) {
 		
- ndom();
-		
+Random randomMaker = new Random();
+ for (int i = 0; i < 10; i++) {
 		int randomNumber = randomMaker.nextInt(5);
 		
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
 if (randomNumber == 1) {
-	JOptionPane.showMessage("your trash kid your 90's are bad get good kid");
+	JOptionPane.showMessageDialog(null,"your trash kid your 90's are bad get good kid");
 }
 if (randomNumber == 2) {
-	JOptionPane.showMessage("your bad kid");
+	JOptionPane.showMessageDialog(null,"your bad kid");
 }
 if (randomNumber == 1) {
-	JOptionPane.showMessage("get good kid");
-}
+	JOptionPane.showMessageDialog(null,"get good kid");
+}}
 	
 	
 		// 2. Repeat all the code above 10 times
+		
+			
 		
 		// 3. Find someone to test out your program. They will like it :)
 	}
--- src/_05_magic_8_ball/Magic8Ball.java
+++ src/_05_magic_8_ball/Magic8Ball.java
@@ -4,20 +4,26 @@
 
 package _05_magic_8_ball;
 
+import java.awt.print.Printable;
 import java.util.Random;
+
+import javax.swing.JOptionPane;
 
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below….
+public static void main(String[] args) {
+	
 
-	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+	// 2. Make a variable that will hold a random number and put a random number into this variable using ""
+	Random ran = new Random();
+	num = ran.nextInt();
 	// 3. Print out this variable
-
+    System.out.println(Number);
 	// 4. Get the user to enter a question for the 8 ball
-
+JOptionPane.showInputDialog("Ask a question already");
 	// 5. If the random number is 0
-
+if ran = 0;
 	// -- tell the user "Yes"
 
 	// 6. If the random number is 1
@@ -32,4 +38,4 @@ public class Magic8Ball {
 
 	// -- write your own answer
 
-}
+}}
