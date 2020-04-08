a99b6fc492cca24e755ee9abddccc56db1cb8337
root <root@f59c6875d73a>
2020-04-08 23:27:38 +0000

    4/8/20

--- src/_99_extra/Fillintheblanks.java
+++ src/_99_extra/Fillintheblanks.java
@@ -1,18 +1,25 @@
+import javax.swing.JOptionPane;
+
 //num=ran.nextInt(1000);
 //num=ran.nextInt(900)
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+// int num1 = 99 + 64
+// int num1 = 99 - 64
+// if (cups < 2){
+//JOptionPane.showMessageDialog(null, "Go back to the store");
+//}
+//String people = JOptionPane.inputDialogue("How many people are you going to give cookies");
+//int Num=Integer.parseInt(people);
+//if (Num > 30){
+//JOptionPane.showMessageDialog(null, "You will need to bake 2 batches");
+//}
+//String batches = JOptionPane.inputDialogue("How many batches did u make");
+//int amount=Integer.parseInt(batches);
+//if ( amount > 2){
+//JOptionPane.showMessageDialog(null, "Your lucky to have so many friends");
+//}
+//printMessage();
+//setName();
+//getNumber();
 //
 //
 //
